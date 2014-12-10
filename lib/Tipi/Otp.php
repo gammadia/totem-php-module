0a8a572692f124cd80bbcd1a9f053ee7a784dbd1
= <=>
2014-12-10 16:38:23 +0100

    Initial commit

---
+++ lib/Tipi/Otp.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tipi\Tipi;
+
+class Otp {
+	/**
+	 *  Taille, en bit, du passe à générer.
+	 *
+	 *  @type {Number}
+	 */
+	private static $key_size = 96;
+
+	/**
+	 *  Base du dernier hash généré.
+	 *
+	 *  @type {Number}
+	 */
+	private $last_time = null;
+
+	/**
+	 *  Dernier code retourné.
+	 *
+	 *  @type {String}
+	 */
+	private $last_hash = null;
+
+	/**
+	 *  Création d'un générateur OTP
+	 *
+	 *  @param   string secret Clef privée, String en base64
+	 *
+	 *  @return Tipi\Tipi\Otp        Générateur OTP
+	 */
+	public function __construct($secret) {
+		if (is_string($secret)) {
+			$secret = base64_decode($secret);
+		}
+
+		$this->secret = $secret;
+	}
+
+	/**
+	 *  Création du code et encodage en base64
+	 *
+	 *  @return string   Code en base64
+	 */
+	public function getCode() {
+		$time = floor(time() / 30);   //  Unix timestamp / 30
+		$hash = null;
+
+		if ($this->last_time !== $time) {
+			$hash = hash_hmac('sha512', $time, $this->secret);
+
+			//  Garde seulement les [key_size] bits les moins significatifs
+			$hash = substr($hash, strlen($hash) - (self::$key_size / 4));
+
+			$this->last_time = $time;
+			$this->last_hash = $hash;
+		} else {
+			$hash = $this->last_hash;
+		}
+
+		return base64_encode(hex2bin($hash));
+	}
+}
---
+++ lib/Tipi/Session.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tipi\Tipi;
+
+use Tipi\Tipi;
+
+class Session {
+	/**
+	 *  Instance du singleton Session.
+	 *
+	 *  @var Tipi\Tipi\Session
+	 */
+	private static $instance = null;
+
+	/**
+	 *  Lecture de l'instance de Session
+	 *
+	 *  @return Tipi\Tipi\Session Instance du singleton
+	 */
+	public static function getInstance() {
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 *  Constructeur privé. (Singleton)
+	 */
+	private function __construct() {}
+
+	/**
+	 *  Id de la session courante.
+	 *
+	 *  @var string
+	 */
+	private static $sessid = null;
+
+	/**
+	 *  Lecture du header Authorization.
+	 *
+	 *  PHP en module, header dans getallheaders().
+	 *  PHP en cgi, header dans $_SERVER['HTTP_AUTHORIZATION'].
+	 *  PHP en fcgi, header dans $_SERVER['REDIRECT_HTTP_AUTHORIZATION'].
+	 *
+	 *  @return string Token d'authentification complet.
+	 */
+	private function getAuthorization() {
+		$auth = '';
+		$headers = getallheaders();
+
+		if (isset($headers['Authorization'])) {
+			$auth = $headers['Authorization'];
+		} else if (isset($headers['authorization'])) {
+			$auth = $headers['authorization'];
+		} else if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+			$auth = $_SERVER['HTTP_AUTHORIZATION'];
+		} else if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+			$auth = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+		}
+
+		return $auth;
+	}
+
+	/**
+	 *  Défini un id de session.
+	 *  Utilisé si l'id est dans la session PHP mais pas dans la requête utilisateur.
+	 *
+	 *  @param string $sessid Id de la session
+	 *  @return Tipi\Tipi\Session $this
+	 */
+	public function setId($sessid) {
+		self::$sessid = $sessid;
+
+		return $this;
+	}
+
+	/**
+	 *  Lecture de l'id de session pour la requête courante.
+	 *
+	 *  @param boolean $force_read Force la lecture dans le header HTTP
+	 *  @return  string      ID de la session
+	 */
+	public function getId($force_read = false) {
+		if ($force_read || self::$sessid === null) {
+			preg_match(
+				'/(?:sessid=")(?P<sessid>[a-z0-9\/+=\-]+)(?:")/i',
+				$this->getAuthorization(),
+				$token
+			);
+
+			if (!isset($token['sessid']) || empty($token['sessid'])) {
+				return null;
+			}
+
+			self::$sessid = bin2hex(base64_decode($token['sessid']));
+		}
+
+		return self::$sessid;
+	}
+
+	/**
+	 *  Vérifie si la session de l'utilisateur est bien active sur le serveur
+	 *
+	 *  @return boolean
+	 */
+	public function isValid() {
+		$result = Tipi::getInstance()->makeRequest('session/ping', 'POST', array(
+			'sess_id' => $this->getId(),
+			'timestamp' => time()
+		));
+
+		$result = json_decode($result, true);
+
+		return isset($result['success']) && $result['success'] === true;
+	}
+}
